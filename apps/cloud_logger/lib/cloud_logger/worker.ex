895517d7127871c7a4d15f56f35a5fd6b98f8314
Christopher Coté <Christopher Cot>
2017-03-31 14:52:38 -0500

    update cloud logger for new cicada histogram interface

--- apps/cloud_logger/lib/cloud_logger/worker.ex
+++ apps/cloud_logger/lib/cloud_logger/worker.ex
@@ -2,6 +2,7 @@ defmodule CloudLogger.Worker do
   use GenServer
   require Logger
   alias Cicada.DeviceManager
+  alias Cicada.DeviceManager.Device.Histogram
 
   def start_link do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
@@ -13,10 +14,15 @@ defmodule CloudLogger.Worker do
   end
 
   def handle_info(:send_data, state) do
-    devices = DeviceManager.Client.snapshot() |> Enum.map(fn device ->
-      %{ device | device: Map.delete(device.device, :device_pid)}
+    devices = DeviceManager.devices |> Enum.flat_map(fn {pid, module, device} ->
+      values = Histogram.snapshot(device.histogram)
+      Histogram.reset(device.histogram)
+      d =
+        module.device(pid)
+        |> Map.delete(:device_pid)
+        |> Map.delete(:histogram)
+      %{values: values, device: d}
     end)
-    DeviceManager.Client.reset_histogram()
     Logger.info "#{inspect devices}"
     devices |> CloudLogger.MQTT.send
     Process.send_after(self(), :send_data, 60*1000)
